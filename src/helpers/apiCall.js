1ad088746d4b9aba7a201e5412b0c86121d17a91
Nicole Latifi <>
2020-11-17 14:43:12 -0700

    Create getRestaurants method that fetches data from api

---
+++ src/helpers/apiCall.js
@@ -0,0 +1,12 @@
+export const getRestaurants = () => {
+  return fetch("https://code-challenge.spectrumtoolbox.com/api/restaurants", {
+    method: 'GET',
+    headers: {
+        Authorization: "Api-Key q3MNxtfep8Gt",
+    }
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data)
+  })
+}
