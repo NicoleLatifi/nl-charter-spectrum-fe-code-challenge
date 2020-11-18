3f8fea1cb581e9553b2c423b00b038ef60e5bfe1
Nicole Latifi <>
2020-11-18 09:51:16 -0700

    Pass all necessary props and add proptypes #10

--- src/Restaurant/Restaurant.js
+++ src/Restaurant/Restaurant.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Restaurant = ({ name }) => {
+const Restaurant = ({ name, city, state, phone, genre }) => {
   return(
     <div >
       <h2>{name}</h2>
+      <p>{city}, {state}</p>
+      <p>{phone}</p>
+      <p>{genre}</p>
     </div>
   )
 }
 
 export default Restaurant;
+
+Restaurant.propTypes = {
+  name: PropTypes.string,
+  city: PropTypes.string,
+  state: PropTypes.string,
+  phone: PropTypes.string,
+  genre: PropTypes.string,
+}
