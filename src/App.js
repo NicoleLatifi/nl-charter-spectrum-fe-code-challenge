36348c78b31a0f67b9317b4e20920167bf923aad
Nicole Latifi <>
2020-11-18 08:58:54 -0700

    Change component name to RestaurantsTable for readability

--- src/App.js
+++ src/App.js
@@ -2,23 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import { getRestaurants } from './helpers/apiCall';
-import Restaurants from './Restaurants'
+import RestaurantsTable from './RestaurantsTable'
 
 function App() {
   const [restaurants, setRestaurants] = useState([]);
 
   useEffect(() => {
-    console.log(restaurants)
     getRestaurants()
       .then(data => setRestaurants(data))
-    console.log(restaurants)
   }, [])
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Restaurants restaurants={restaurants}/>
+        <RestaurantsTable restaurants={restaurants}/>
         <a
           className="App-link"
           href="https://reactjs.org"
