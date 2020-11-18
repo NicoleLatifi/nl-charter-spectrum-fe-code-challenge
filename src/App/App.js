da5697f707259d551efd1392db9862dff2935932
Nicole Latifi <>
2020-11-18 12:52:04 -0700

    Create determineStates method  and pass states as prop to FilterByState #15

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,43 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import { getRestaurants } from '../helpers/apiCall';
-import RestaurantsTable from '../RestaurantsTable/RestaurantsTable'
+import RestaurantsTable from '../RestaurantsTable/RestaurantsTable';
+import FilterByState from '../FilterByState/FilterByState';
 
 function App() {
   const [restaurants, setRestaurants] = useState([]);
+  const [states, setStates] = useState([])
 
   useEffect(() => {
     getRestaurants()
       .then(data => setRestaurants(data))
   }, [])
 
+  useEffect(() => {
+    determineStates()
+  }, [restaurants])
+
+  function determineStates() {
+    const allStates = []
+    restaurants.forEach(restaurant => {
+      console.log("hi")
+      if (!allStates.includes(restaurant.state)) {
+        allStates.push(restaurant.state)
+      }
+    })
+    allStates.sort(function(a, b){
+      if(a < b) { return -1; }
+      if(a > b) { return 1; }
+      return 0;
+    })
+    setStates(allStates)
+  }
+
   return (
     <div className="App">
       <h1 className="heading">Restaurants</h1>
-      <RestaurantsTable restaurants={restaurants}/>
+      <RestaurantsTable restaurants={restaurants} />
+      <FilterByState states={states} />
     </div>
   )
 }
