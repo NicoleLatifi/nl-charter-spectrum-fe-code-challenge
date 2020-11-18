d40fab2f146ef37d3d3466a3d5750c9bec3f0e6b
Nicole Latifi <>
2020-11-18 08:49:56 -0700

    Get restaurant data and pass as props to rRestaurants component #2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
-import { getRestaurants } from './helpers/apiCall'
+import React, { useState, useEffect } from 'react';
+import { getRestaurants } from './helpers/apiCall';
+import Restaurants from './Restaurants'
 
 function App() {
+  const [restaurants, setRestaurants] = useState([]);
 
   useEffect(() => {
+    console.log(restaurants)
     getRestaurants()
-  });
+      .then(data => setRestaurants(data))
+    console.log(restaurants)
+  }, [])
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Restaurants restaurants={restaurants}/>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -26,7 +29,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
