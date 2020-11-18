cf5fc84e09f5288f2bb2e3da5941f41ca1667fec
Nicole Latifi <>
2020-11-18 10:32:46 -0700

    Remove unnecessary boilerplate

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import { getRestaurants } from '../helpers/apiCall';
@@ -14,18 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <RestaurantsTable restaurants={restaurants}/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RestaurantsTable restaurants={restaurants}/>
     </div>
   )
 }
