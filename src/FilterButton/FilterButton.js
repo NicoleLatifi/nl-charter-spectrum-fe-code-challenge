f91c5e7a3310b24a23ec27f1717c7a8c33bc41e9
Nicole Latifi <>
2020-11-18 20:18:17 -0700

    Rename function to updateStatesSelected #20

--- src/FilterButton/FilterButton.js
+++ src/FilterButton/FilterButton.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types';
 import './FilterButton.css'
 
-const FilterButton = ({ state, updateFilter }) => {
+const FilterButton = ({ state, updateStatesSelected }) => {
   const [isSelected, setIsSelected] = useState(false)
 
   const handleClick = (event) => {
     console.log('hi')
     setIsSelected(!isSelected)
-    updateFilter(event.target.innerText)
+    updateStatesSelected(event.target.innerText)
   }
 
   return (
@@ -22,5 +22,5 @@ export default FilterButton
 
 FilterButton.propTypes = {
   state: PropTypes.string,
-  updateFilter: PropTypes.func,
+  updateStatesSelected: PropTypes.func,
 }
