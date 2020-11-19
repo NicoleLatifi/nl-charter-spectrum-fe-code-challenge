7716892fb9b18988ba8d24942b8bde118e7e90ef
Nicole Latifi <>
2020-11-18 21:58:08 -0700

    Add determineGenres function and render FilterByGenre component #24

--- src/App/App.js
+++ src/App/App.js
@@ -3,12 +3,15 @@ import React, { useState, useEffect } from 'react';
 import { getRestaurants } from '../helpers/apiCall';
 import RestaurantsTable from '../RestaurantsTable/RestaurantsTable';
 import FilterByState from '../FilterByState/FilterByState';
+import FilterByGenre from '../FilterByGenre/FilterByGenre';
 
 function App() {
   const [restaurants, setRestaurants] = useState([])
+  const [restaurantsToDisplay, setRestaurantsToDisplay] = useState([])
   const [states, setStates] = useState([])
   const [statesSelected, setStatesSelected] = useState([])
-  const [restaurantsToDisplay, setRestaurantsToDisplay] = useState([])
+  const [genres, setGenres] = useState([])
+  const [genresSelected, setGenresSelected] = useState([])
 
   useEffect(() => {
     getRestaurants()
@@ -21,13 +24,14 @@ function App() {
 
   useEffect(() => {
     determineStates()
+    determineGenres()
   }, [restaurants])
 
   useEffect(() => {
     filterRestaurants()
   }, [statesSelected])
 
-  function determineStates() {
+  const determineStates = () => {
     const allStates = []
     restaurants.forEach(restaurant => {
       if (!allStates.includes(restaurant.state)) {
@@ -40,6 +44,24 @@ function App() {
       return 0;
     })
     setStates(allStates)
+  }
+
+  const determineGenres = () => {
+    const allGenres = []
+    restaurants.forEach(restaurant => {
+      const genresList = restaurant.genre.split(",")
+      genresList.forEach(genre => {
+        if (!allGenres.includes(genre)) {
+          allGenres.push(genre)
+        }
+      })
+    })
+    allGenres.sort(function(a, b){
+      if(a < b) { return -1; }
+      if(a > b) { return 1; }
+      return 0;
+    })
+    setGenres(allGenres)
   }
 
   const filterRestaurants = () => {
@@ -64,12 +86,17 @@ function App() {
     }
   }
 
+  const updategenresSelected = () => {
+    console.log("update genre")
+  }
+
   return (
     <div>
       <h1 className="title">Restaurants</h1>
       <div className="table-section">
         <RestaurantsTable restaurantsToDisplay={restaurantsToDisplay} statesSelected={statesSelected} className="table-container" />
         <FilterByState states={states} updateStatesSelected={updateStatesSelected} className="filter-by-state-container"/>
+        <FilterByGenre genres={genres} updategenresSelected={updategenresSelected} className="filter-by-genre-container"/>
       </div>
     </div>
   )
